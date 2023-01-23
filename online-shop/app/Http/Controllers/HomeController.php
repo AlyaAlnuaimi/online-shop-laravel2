ab4c8aa7db1e2e83cc80dd9a95ee1fe45bc8a1ef
AlyaAlnuaimi <aliaalnuaimi111@gmail.com>
2023-01-23 16:58:36 +0400

    laravel

---
+++ online-shop/app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    function index()
+    {
+        //take data from the database
+        return view('index')->with('categories', Category::all());
+    }
+
+    function shop()
+    {
+        return view('shop');
+    }
+}
