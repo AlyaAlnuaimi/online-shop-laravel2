d5d03a2f7bc2ebdb742250fa63f7034c1c6b712c
AlyaAlnuaimi <aliaalnuaimi111@gmail.com>
2023-01-25 16:43:29 +0400

    laravel

--- online-shop/routes/web.php
+++ online-shop/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\CheckProductsInSession;
+use App\Http\Middleware\OrdersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +23,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/add-product', [HomeController::class, 'add_product']);
 Route::get('/shop', [HomeController::class, 'shop']);
+// order 
+Route::get('/cart', [OrdersController::class, 'cart'])->name('cart');
+Route::get('/productServices', [OrdersController::class, 'productServices']);
+
+
 Route::get('/contact',[ContactController::class,'index'])->name('contact');
 Route::get('/checkout',[ProductsController::class,'checkout'])->middleware(CheckProductsInSession::class);
 
@@ -43,3 +49,4 @@ Route::middleware(['auth', 'can:is_admin'])->prefix('/admin')->group(function ()
     Route::resource('products', ProductsController::class);
     Route::resource('categories', CategoriesController::class);
 });
+
