eade1dde412331182cc237798cc280c570c76553
AlyaAlnuaimi <aliaalnuaimi111@gmail.com>
2023-01-25 13:09:05 +0400

    laravel

--- online-shop/routes/web.php
+++ online-shop/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckProductsInSession;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,13 +19,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/add-product', [HomeController::class, 'add_product']);
 Route::get('/shop', [HomeController::class, 'shop']);
 Route::get('/contact',[ContactController::class,'index'])->name('contact');
-Route::post('/contact',[ContactController::class,'sendMessage']);
+Route::get('/checkout',[ProductsController::class,'checkout'])->middleware(CheckProductsInSession::class);
 
-/*
+Route::post('/contact',[ContactController::class,'sendMessage']);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -34,10 +35,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-/*
+
 require __DIR__ . '/auth.php';
 
+// require log in to preview the content of products and categories
 Route::middleware(['auth', 'can:is_admin'])->prefix('/admin')->group(function () {
     Route::resource('products', ProductsController::class);
     Route::resource('categories', CategoriesController::class);
-});*/
+});
